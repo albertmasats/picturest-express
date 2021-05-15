f4bf80f7c2772aabd75ea21c93edebfa7cb55b14
Facundo Farias <facundo@happets.com>
2021-05-15 10:35:19 +0200

    Adding router and controller for pins

---
+++ src/pins/pins.router.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const pinsController = require('./pins.controller');
+
+router
+  .route('/')
+    .get(pinsController.all)
+    .post(pinsController.create);
+
+router
+  .route('/:id')
+    .get(pinsController.get)
+    .delete(pinsController.delete)
+    .put(pinsController.update);
+
+module.exports = router;
