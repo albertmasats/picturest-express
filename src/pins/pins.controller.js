8be83f39950c54143bff749338e0009368e4cf1d
Angel Blanco <angelbt91@gmail.com>
2021-06-03 13:07:09 +0200

    Add userId saving on pin creation + Styling fixes

--- src/pins/pins.controller.js
+++ src/pins/pins.controller.js
@@ -1,25 +1,36 @@
 const pinModel = require("./pins.model");
+const jwt = require("jsonwebtoken");
 
 const all = async (req, res) => {
-  const pins = await pinModel.getAll();
-  res.json(pins);
+    const pins = await pinModel.getAll();
+    res.json(pins);
 };
 
 const create = async (req, res) => {
-  const pin = await pinModel.create(req.body);
-  res.status(201).json(pin);
+    const token = req.headers.authorization.split(" ")[1];
+    const tokenDecoded = jwt.decode(token);
+
+    const pin = await pinModel.create({
+        ...req.body,
+        userId: tokenDecoded.id
+    });
+
+    res.status(201).json(pin);
 };
 
-const get = async (req, res) => {};
+const get = async (req, res) => {
+};
 
-const update = async (req, res) => {};
+const update = async (req, res) => {
+};
 
-const remove = async (req, res) => {};
+const remove = async (req, res) => {
+};
 
 module.exports = {
-  all,
-  create,
-  get,
-  update,
-  remove,
+    all,
+    create,
+    get,
+    update,
+    remove,
 };
