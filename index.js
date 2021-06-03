1773c52d12ea6dbabd6107a79b75a914427dc90a
Angel Blanco <angelbt91@gmail.com>
2021-06-03 12:53:04 +0200

    Fix bodyParser use + styling fixes

--- index.js
+++ index.js
@@ -1,35 +1,26 @@
+require('dotenv').config();
 const express = require("express");
-const { json, urlencoded } = require("body-parser");
+const {json, urlencoded} = require("body-parser");
 const morgan = require("morgan");
 const cors = require("cors");
 const path = require("path");
 const pins = require("./src/pins/pins.router");
 const users = require("./src/user/users.router");
 const auth = require("./src/auth/auth.router");
-require('dotenv').config();
-
 const mongoose = require("mongoose");
 
-const options = { useNewUrlParser: true, useUnifiedTopology: true };
-const mongo = mongoose.connect(
-  process.env.DB_HOST,
-  options
-);
-
-mongo.then(() => {
-  console.log("Mongo ready to accept queries");
+const options = {useNewUrlParser: true, useUnifiedTopology: true};
+mongoose.connect(process.env.DB_HOST, options, () => {
+    console.log("Mongo ready to accept queries");
 });
 
 global.appRoot = path.resolve(__dirname);
-const app = express();
 
-app.use("/pins", pins);
-app.use("/users", users);
-app.use("/auth", auth);
+const app = express();
 
 app.use(cors());
 app.use(json());
-app.use(urlencoded({ extended: true }));
+app.use(urlencoded({extended: true}));
 app.use(morgan("dev"));
 app.disable("x-powered-by Nuclio");
 
@@ -38,13 +29,13 @@ app.use("/users", users);
 app.use("/auth", auth);
 
 const start = async () => {
-  try {
-    app.listen(5000, () => {
-      console.log(`REST API on http://localhost:5000/`);
-    });
-  } catch (e) {
-    console.error(e);
-  }
+    try {
+        app.listen(5000, () => {
+            console.log(`REST API on http://localhost:5000/`);
+        });
+    } catch (e) {
+        console.error(e);
+    }
 };
 
 start();
