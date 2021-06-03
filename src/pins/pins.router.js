5464141c2d189f30957b9356deb3c9c37a6e9a88
Angel Blanco <angelbt91@gmail.com>
2021-06-03 12:57:42 +0200

    Add token middleware + Styling fixes

--- src/pins/pins.router.js
+++ src/pins/pins.router.js
@@ -1,13 +1,31 @@
 const express = require("express");
 const router = express.Router();
 const pinsController = require("./pins.controller");
+const jwt = require("jsonwebtoken");
 
-router.route("/").get(pinsController.all).post(pinsController.create);
+const middleware = async (req, res, next) => {
+    if (!req.headers.authorization) {
+        return res.status(401).json("Forbidden.");
+    }
 
-router
-  .route("/:id")
-  .get(pinsController.get)
-  .delete(pinsController.remove)
-  .put(pinsController.update);
+    const token = req.headers.authorization.split(' ')[1];
+
+    jwt.verify(token, process.env.TOKEN_SECRET, (err) => {
+        if (err) {
+            return res.status(401).json(err)
+        }
+
+        return next();
+    });
+}
+
+router.route("/")
+    .get(pinsController.all)
+    .post(middleware, pinsController.create);
+
+router.route("/:id")
+    .get(pinsController.get)
+    .delete(pinsController.remove)
+    .put(pinsController.update);
 
 module.exports = router;
