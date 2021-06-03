3604994166ab7c46257a0461bd3d8d8179e09d87
Angel Blanco <angelbt91@gmail.com>
2021-06-03 12:59:03 +0200

    Add token middleware + userId field to Pin schema + Styling fixes

--- src/auth/auth.controller.js
+++ src/auth/auth.controller.js
@@ -2,26 +2,24 @@ const userModel = require("../user/users.model");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 
-
 const login = async (req, res) => {
-
     const user = await userModel.search({
         email: req.body.email,
-    })
+    });
 
     if (user) {
         const result = await bcrypt.compare(req.body.password, user.password);
-            if (result) {
-                const token = jwt.sign({user},process.env.TOKEN_SECRET);
-                return res.json(token);
-            } else {
-                return res.status(401).json("wrong password")
-            }
-        
-    }
-    return res.status(400).json('Not found');
-}
 
+        if (result) {
+            const token = jwt.sign({id: user._id}, process.env.TOKEN_SECRET);
+            return res.json(token);
+        } else {
+            return res.status(401).json("Wrong email or password.");
+        }
+    }
+
+    return res.status(401).json("Wrong email or password.");
+}
 
 module.exports = {
     login
--- src/pins/pins.model.js
+++ src/pins/pins.model.js
@@ -1,24 +1,25 @@
 const mongoose = require("mongoose");
 
 const PinSchema = new mongoose.Schema({
-  name: String,
-  urlImg: String,
-  subTitle: String,
+    name: String,
+    urlImg: String,
+    subTitle: String,
+    userId: mongoose.Schema.Types.ObjectId
 });
 
 const PinModel = mongoose.model("pins", PinSchema);
 
 const create = async (pin) => {
-  const pinCreated = await PinModel.create(pin);
-  return pinCreated;
+    const pinCreated = await PinModel.create(pin);
+    return pinCreated;
 };
 
 const getAll = async () => {
-  const pins = await PinModel.find();
-  return pins;
+    const pins = await PinModel.find();
+    return pins;
 };
 
 module.exports = {
-  create,
-  getAll,
+    create,
+    getAll,
 };
